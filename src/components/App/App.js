6bdf96f93a885c5d2f146bd2a510d2f8fd67beea
BJ Pennington <bjpennington112@gmail.com>
2018-08-05 15:02:50 -0500

    set up redux router in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import './App.css';
+
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+
 import Header from '../Header/Header';
 import AdminTable from '../AdminTable/AdminTable';
 import FeedbackCard from '../FeedbackCard/FeedbackCard';
+import NotFound from '../NotFound/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <AdminTable />
-        <FeedbackCard />
-        <br/>
+        <Router>
+          <Switch>
+            <Route path="/admin" component={AdminTable} />
+            <Route path="/feedback_pg_2" component={FeedbackCard} />
+            <Redirect exact from="/" to="/feedback_pg_2" />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
       </div>
     );
   }
