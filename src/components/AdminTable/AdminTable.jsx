a372ad0645cfb5c96436ae3e8f544d16934af6b5
BJ Pennington <bjpennington112@gmail.com>
2018-08-04 22:42:11 -0500

    map feedback data to table rows in admin table component

--- src/components/AdminTable/AdminTable.jsx
+++ src/components/AdminTable/AdminTable.jsx
@@ -1,22 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 class AdminTable extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            feedback: []
+        }
     }
 
     getFeedback = () => {
         axios.get('/api/feedback')
-                .then(response => {
-                    this.setState({
-                        feedback: response.data
-                    });
-                })
-                .catch(error => {
-                    console.log('Error on /api/feedback GET:', error);
+            .then(response => {
+                this.setState({
+                    feedback: response.data
                 });
+            })
+            .catch(error => {
+                console.log('Error on /api/feedback GET:', error);
+            });
     }
 
     componentDidMount() {
@@ -24,10 +26,55 @@ class AdminTable extends Component {
     }
 
     render() {
-        console.log(this.state);
-        
+        let feedbackMapArray = this.state.feedback.map((feedback, index) => {
+            return (
+                <tr key={index}>
+                    <td>
+                        {feedback.feeling}
+                    </td>
+                    <td>
+                        {feedback.understanding}
+                    </td>
+                    <td>
+                        {feedback.support}
+                    </td>
+                    <td>
+                        {feedback.comments}
+                    </td>
+                    <td>
+                        <button>
+                            Delete
+                    </button>
+                    </td>
+                </tr>
+            )
+        })
+
         return (
-            <p>{JSON.stringify(this.state)}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Feeling
+                        </th>
+                        <th>
+                            Comprehension
+                        </th>
+                        <th>
+                            Support
+                        </th>
+                        <th>
+                            Comments
+                        </th>
+                        <th>
+                            Delete
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {feedbackMapArray}
+                </tbody>
+            </table>
         )
     }
 }
